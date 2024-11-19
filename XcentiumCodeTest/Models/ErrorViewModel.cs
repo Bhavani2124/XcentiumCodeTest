17968a19057ba11459d0fa46889c63b48eab3397
Siva Durga Bhavani Pandrinki <sivadurgabhavani.pandrinki@carestream.com>
2024-11-19 19:23:27 +0530

    Add project files.

---
+++ XcentiumCodeTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XcentiumCodeTest.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
